4b139fdc2dd029b0153c7b3c7a3b0d25a167ac9c
sindhujaDeveloper <crazydeveloper.p@gmail.com>
2023-12-13 11:01:32 +0530

    env file added into gitignore - ai project frontend

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,7 @@
 
+
+// VITE_APP_GOOGLE_SIGNIN_CLIENT_ID = "367767237355-044o2mi3crvfdr68haiiijiki6vf8fft.apps.googleusercontent.com"
+// VITE_OPEN_AI_KEY="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"
 export const config = {
   googleSignInClientId: process.env.VITE_APP_GOOGLE_SIGNIN_CLIENT_ID || '',
   // Other configuration properties...
