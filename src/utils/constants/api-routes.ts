be610eba3cdca01058dce3250cd3ab93684d75ac
Sindhuja-Axim <sindhuja.p@aximsoft.com>
2024-06-21 15:20:01 +0530

    api route changes

--- src/utils/constants/api-routes.ts
+++ src/utils/constants/api-routes.ts
@@ -8,10 +8,10 @@ const enum HttpMethods {
 
 export const API = {
   /* Auth */
-  login: { apiPath: "https://chatbot-with-login.netlify.app/public/auth/login", action: HttpMethods.POST },
-  signup: { apiPath: "https://chatbot-with-login.netlify.app/public/auth/signup", action: HttpMethods.POST },
-  forgetPassword: { apiPath: "https://chatbot-with-login.netlify.app/public/auth/forgot-password", action: HttpMethods.POST },
-  resetPassword: { apiPath: "https://chatbot-with-login.netlify.app/public/auth/reset-password", action: HttpMethods.POST },
-  conversationChatbot: { apiPath: "https://chatbot-with-login.netlify.app/private/chatbot/conversation", action: HttpMethods.POST },
-  textToImageChatbot: { apiPath: "https://chatbot-with-login.netlify.app/private/chatbot/textToImage", action: HttpMethods.POST }
+  login: { apiPath: "https://chatbot-with-login.netlify.app/api/public/auth/login", action: HttpMethods.POST },
+  signup: { apiPath: "https://chatbot-with-login.netlify.app/api/public/auth/signup", action: HttpMethods.POST },
+  forgetPassword: { apiPath: "https://chatbot-with-login.netlify.app/api/public/auth/forgot-password", action: HttpMethods.POST },
+  resetPassword: { apiPath: "https://chatbot-with-login.netlify.app/api/public/auth/reset-password", action: HttpMethods.POST },
+  conversationChatbot: { apiPath: "https://chatbot-with-login.netlify.app/api/private/chatbot/conversation", action: HttpMethods.POST },
+  textToImageChatbot: { apiPath: "https://chatbot-with-login.netlify.app/api/private/chatbot/textToImage", action: HttpMethods.POST }
 };
